de9943f36bd429dcf9e859f30093f46796fdd38d
myxy99 <2399158611@qq.com>
2021-01-03 13:36:59 +0800

    :tada:first commit

---
+++ cmd/getway/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/myxy99/component/pkg/xcolor"
+	"github.com/myxy99/component/pkg/xflag"
+	"github.com/myxy99/component/pkg/xsignals"
+	"ndisk/cmd/getway/app"
+	"os"
+)
+
+func main() {
+	xflag.Register(
+		xflag.CommandNode{
+			Name: "run",
+			Command: &xflag.Command{
+				Use:   "run",
+				Short: "run your app",
+				Run: func(cmd *xflag.Command, args []string) {
+					err := app.Run(xsignals.SetupSignalHandler())
+					if err != nil {
+						fmt.Println(xcolor.Red(err.Error()))
+						os.Exit(1)
+					}
+				},
+			},
+			Flags: func(c *xflag.Command) {
+				c.Flags().StringP("xcfg", "c", "", "配置文件")
+				_ = c.MarkFlagRequired("xcfg")
+			},
+		},
+	)
+	_ = xflag.Parse()
+}
---
+++ internal/getway/server.go
@@ -0,0 +1,90 @@
+package getway
+
+import (
+	"context"
+	"github.com/BurntSushi/toml"
+	"github.com/myxy99/component/pkg/xconsole"
+	"github.com/myxy99/component/pkg/xdefer"
+	"github.com/myxy99/component/pkg/xflag"
+	"github.com/myxy99/component/pkg/xvalidator"
+	"github.com/myxy99/component/xcfg"
+	"github.com/myxy99/component/xcfg/datasource/manager"
+	"github.com/myxy99/component/xgovern"
+	"ndisk/internal/getway/api/v1/registry"
+	myValidator "ndisk/internal/getway/validator"
+	"net/http"
+)
+
+type Server struct {
+	Server *http.Server
+	err    error
+}
+
+func (s *Server) PrepareRun(stopCh <-chan struct{}) (err error) {
+	s.initCfg()
+	s.initHttpServer()
+	s.initRouter()
+	s.initValidator()
+	s.govern()
+	return s.err
+}
+
+func (s *Server) Run(stopCh <-chan struct{}) (err error) {
+	go func() {
+		<-stopCh
+		xdefer.Clean()
+	}()
+	xdefer.Register(func() error {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		xconsole.Red("http server Shutdown")
+		return s.Server.Shutdown(ctx)
+	})
+	xconsole.Greenf("Start listening on:", s.Server.Addr)
+	err = s.Server.ListenAndServe()
+	if err != http.ErrServerClosed {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) initCfg() {
+	if s.err != nil {
+		return
+	}
+	var data xcfg.DataSource
+	data, s.err = manager.NewDataSource(xflag.NString("run", "xcfg"))
+	if s.err != nil {
+		return
+	}
+	s.err = xcfg.LoadFromDataSource(data, toml.Unmarshal)
+}
+
+func (s *Server) initHttpServer() {
+	if s.err != nil {
+		return
+	}
+	s.Server = new(http.Server)
+	s.Server.Addr = xcfg.GetString("server.addr")
+}
+
+func (s *Server) initRouter() {
+	if s.err != nil {
+		return
+	}
+	s.Server.Handler = registry.Engine()
+}
+
+func (s *Server) initValidator() {
+	if s.err != nil {
+		return
+	}
+	s.err = xvalidator.Init(xcfg.GetString("server.locale"), myValidator.RegisterValidation)
+}
+
+func (s *Server) govern() {
+	if s.err != nil {
+		return
+	}
+	go xgovern.Run()
+}
